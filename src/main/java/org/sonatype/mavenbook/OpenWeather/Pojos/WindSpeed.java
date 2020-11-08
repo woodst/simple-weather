64f22fc40b7bb82493d3b7f8451f8aec84de6606
woodst <woodst@wodsnet.org>
2020-11-07 17:05:30 -0800

    Initial Commit
    Includes abandoned Yahoo Weather and embraces OpenWeather.

--- src/main/java/org/sonatype/mavenbook/OpenWeather/Pojos/WindSpeed.java
+++ src/main/java/org/sonatype/mavenbook/OpenWeather/Pojos/WindSpeed.java
@@ -1,4 +1,41 @@
 package org.sonatype.mavenbook.OpenWeather.Pojos;
 
+/**
+ *  OpenWeather Wind speed information
+ *
+ * @author Tom Woods
+ * @version 0.01
+ * @since 2020-11-07
+ */
 public class WindSpeed {
+
+    private Float value;
+    private String unit;
+    private String name;
+
+    public WindSpeed() { }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
